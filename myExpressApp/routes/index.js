7f2d0b2375e9838371c92ec600976c368c7d0c0d
mossuchida <12548207+mossuchida@users.noreply.github.com>
2023-12-23 17:20:18 -0500

    add counts and hostname

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var number = 0;
+var os = require("os");
+var hostname = os.hostname();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BYU Idaho!' });
+  number += 1;
+  res.render('index', { title: 'BYU Idaho!', count: number, hostname: hostname});
 });
 
 module.exports = router;
